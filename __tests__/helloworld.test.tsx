32629c9206ddff66e64de9ef1864210de2c0f690
akhilvc10 <akhilvc10@gmail.com>
2022-02-23 14:23:25 +0530

    fix(): updated test file

--- __tests__/helloworld.test.tsx
+++ __tests__/helloworld.test.tsx
@@ -1,15 +1,10 @@
-import {
-  render,
-  cleanup,
-  screen,
-  act,
-  waitFor,
-  fireEvent
-} from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
-import HelloWorld from "../src/components/HelloWorld";
+
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { wait } from "@testing-library/user-event/dist/utils";
+
+import HelloWorld from "../src/components/HelloWorld";
 
 afterAll(() => {
   cleanup();
