ab39b061e7ed0b14be2cd426d8b6da014eb43c18
akhilvc10 <akhilvc10@gmail.com>
2022-02-23 15:14:50 +0530

    fix(): updated image

--- cypress/integration/helloworld.test.ts
+++ cypress/integration/helloworld.test.ts
@@ -1,21 +1,18 @@
 /// <reference types="cypress" />
 
-describe('My First Test', () => {
-    before(function () {
-        cy.visit('http://localhost:3000/')
-    })
-    it('Should render hello world', () => {
+describe("My First Test", () => {
+  before(function () {
+    cy.visit("http://localhost:3000/");
+  });
 
-        cy.contains('Hello world')
-    })
-    
-    
-        it('File Upload using cypress-file-upload npm package', () => {
-            const filepath = '../fixtures/example.jpg'
-            cy.get('input[type="file"]').attachFile(filepath)
-            // cy.get('#file-submit').click()
-   
-            cy.get('.MuiAlert-message', { timeout: 8000 }).should('have.text', 'File Uploaded Successfully')
-        })
+  it("File Upload using cypress-file-upload npm package", () => {
+    const filepath = "../fixtures/example.jpg";
+    cy.get('input[type="file"]').attachFile(filepath);
+    // cy.get('#file-submit').click()
 
-  })
+    cy.get(".MuiAlert-message", { timeout: 8000 }).should(
+      "have.text",
+      "File Uploaded Successfully"
+    );
+  });
+});
