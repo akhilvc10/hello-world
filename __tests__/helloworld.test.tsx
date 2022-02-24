0d6198257d39a972dc32babdefdd9aea4aecefd3
akhilvc10 <akhilvc10@gmail.com>
2022-02-24 13:27:16 +0530

    updated test case

--- __tests__/helloworld.test.tsx
+++ __tests__/helloworld.test.tsx
@@ -1,28 +1,27 @@
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom';
 
-import { cleanup, render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { wait } from "@testing-library/user-event/dist/utils";
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 
-import HelloWorld from "../src/components/HelloWorld";
+import HelloWorld from '../src/components/HelloWorld';
 
 afterAll(() => {
   cleanup();
 });
 
-// test("Should render helloworld component", () => {
-//   render(<HelloWorld />);
-//   const testEl = screen.getByTestId("hello");
-//   expect(testEl).toBeInTheDocument();
-//   expect(testEl).toHaveTextContent("Hello world");
-// });
-test("should upload the file", () => {
-  const file = new File(["hello"], "hello.png", { type: "image/png" });
+jest.mock('axios');
+test('should upload the file', () => {
+  const file = new File(['hello'], 'hello.png', { type: 'image/png' });
   render(<HelloWorld />);
-
-  const input = screen.getByTestId("element");
+  axios.post.mockResolvedValue({
+    status: 'success'
+  });
+  const input = screen.getByTestId('element');
   userEvent.upload(input, file);
-  wait(5000);
+  axios.put.mockResolvedValue({
+    status: 200
+  });
   expect(input.files[0]).toEqual(file);
   expect(input.files).toHaveLength(1);
   expect(screen.getByText(/hello\.png/)).toBeInTheDocument();
